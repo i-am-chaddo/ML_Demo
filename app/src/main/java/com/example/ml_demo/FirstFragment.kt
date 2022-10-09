775afcbd44bfd49428827637f2cd00f0dd149313
sagar.vaghela <sagar.vaghela@oracle.com>
2022-10-09 22:57:21 +0530

    Some Progress.

--- app/src/main/java/com/example/ml_demo/FirstFragment.kt
+++ app/src/main/java/com/example/ml_demo/FirstFragment.kt
@@ -1,21 +1,21 @@
 package com.example.ml_demo
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.ml_demo.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+
 class FirstFragment : Fragment() {
 
-//    val textView_result : TextView =
-
+    private val pickImage = 100
+    private var imageUri: Uri? = null
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -35,8 +35,27 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Setting default image
+        binding.imageviewToBeProcessed.setImageResource(R.drawable.azad_profile)
+
         binding.buttonStart.setOnClickListener {
-            binding.textViewResult.text = "ML Tries To Process The Above Image."
+
+            // image pickup
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+
+            // convert image URI to inputImage
+//            var imageForMLModel = imageUri?.let { it1 -> InputImage.fromFilePath(requireContext(), it1) }
+
+            // text detection code
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            binding.imageviewToBeProcessed.setImageURI(imageUri)
         }
     }
 
