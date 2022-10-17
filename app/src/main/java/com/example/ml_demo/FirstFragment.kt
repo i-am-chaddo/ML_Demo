b53bf9eaa7c7c162a2b3d0cbee4fa781edb4a232
sagar.vaghela <sagar.vaghela@oracle.com>
2022-10-17 21:29:42 +0530

    Changes & Object Detection Pending.

--- app/src/main/java/com/example/ml_demo/FirstFragment.kt
+++ app/src/main/java/com/example/ml_demo/FirstFragment.kt
@@ -3,6 +3,7 @@ package com.example.ml_demo
 import android.Manifest
 import android.app.Activity
 import android.app.Activity.RESULT_OK
+import android.app.ProgressDialog
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -19,6 +20,8 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.ml_demo.databinding.FragmentFirstBinding
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import java.util.*
@@ -27,13 +30,15 @@ import java.util.*
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-    private val pickImage = 100
-    private val captureImage = 200
+    private val pickImageCode = 100
+    private val textDetectionCode = 200
+    private val objectDetectionCode = 300
     private val readExt = 300
     private val writeExt = 400
     private var imageUri: Uri? = null
     private var textFromPickedImage: String? = null
-    private lateinit var tts : TextToSpeech
+    private lateinit var tts: TextToSpeech
+    private lateinit var processDialog: ProgressDialog
 
     private val binding get() = _binding!!
 
@@ -46,48 +51,84 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.imageviewToBeProcessed.setImageResource(R.drawable.sneha_profile)
 
         setupPermissions()
 
-        binding.buttonGallery.setOnClickListener {
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            @Suppress("DEPRECATION") startActivityForResult(gallery, pickImage)
+        processDialog = ProgressDialog(context)
+        processDialog.setTitle("Please Wait")
+        processDialog.setCanceledOnTouchOutside(false)
+
+        binding.buttonTextDetection.setOnClickListener {
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.TITLE, "CAP_IMG")
+            values.put(MediaStore.Images.Media.DESCRIPTION, "Captured Image From Camera.")
+            imageUri = context?.contentResolver?.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values
+            )
+            val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            camera.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            @Suppress("DEPRECATION") startActivityForResult(camera, textDetectionCode)
         }
 
-        binding.buttonCamera.setOnClickListener {
+        binding.buttonObjectDetection.setOnClickListener {
             val values = ContentValues()
-            values.put(MediaStore.Images.Media.TITLE,"CAP_IMG")
-            values.put(MediaStore.Images.Media.DESCRIPTION,"Captured Image From Camera.")
-            imageUri = context?.contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            values.put(MediaStore.Images.Media.TITLE, "CAP_IMG")
+            values.put(MediaStore.Images.Media.DESCRIPTION, "Captured Image From Camera.")
+            imageUri = context?.contentResolver?.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values
+            )
             val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            camera.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
-            @Suppress("DEPRECATION") startActivityForResult(camera, captureImage)
+            camera.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            @Suppress("DEPRECATION") startActivityForResult(camera, objectDetectionCode)
         }
     }
 
     private fun setupPermissions() {
-        val pick = context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.READ_MEDIA_IMAGES) }
-        val capture = context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.CAMERA) }
-        val readExternal = context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.READ_EXTERNAL_STORAGE) }
-        val writeExternal = context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.WRITE_EXTERNAL_STORAGE) }
-        if ((pick != PackageManager.PERMISSION_GRANTED) || (capture != PackageManager.PERMISSION_GRANTED) || (readExternal != PackageManager.PERMISSION_GRANTED) || (writeExternal != PackageManager.PERMISSION_GRANTED)) {
+        val pick = context?.let {
+            ContextCompat.checkSelfPermission(
+                it, Manifest.permission.READ_MEDIA_IMAGES
+            )
+        }
+        val capture = context?.let {
+            ContextCompat.checkSelfPermission(
+                it, Manifest.permission.CAMERA
+            )
+        }
+        val readExternal = context?.let {
+            ContextCompat.checkSelfPermission(
+                it, Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+        val writeExternal = context?.let {
+            ContextCompat.checkSelfPermission(
+                it, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
+        val manageExternal = context?.let {
+            ContextCompat.checkSelfPermission(
+                it, Manifest.permission.MANAGE_EXTERNAL_STORAGE
+            )
+        }
+        if ((pick != PackageManager.PERMISSION_GRANTED) || (capture != PackageManager.PERMISSION_GRANTED) || (readExternal != PackageManager.PERMISSION_GRANTED) || (writeExternal != PackageManager.PERMISSION_GRANTED) || (manageExternal != PackageManager.PERMISSION_GRANTED)) {
             makeRequest()
         }
     }
 
     private fun makeRequest() {
         ActivityCompat.requestPermissions(
-            context as Activity, arrayOf(Manifest.permission.READ_MEDIA_IMAGES), pickImage
+            context as Activity, arrayOf(Manifest.permission.READ_MEDIA_IMAGES), pickImageCode
         )
         ActivityCompat.requestPermissions(
-            context as Activity, arrayOf(Manifest.permission.CAMERA), captureImage
+            context as Activity, arrayOf(Manifest.permission.CAMERA), textDetectionCode
         )
         ActivityCompat.requestPermissions(
             context as Activity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), readExt
         )
         ActivityCompat.requestPermissions(
             context as Activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), writeExt
+        )
+        ActivityCompat.requestPermissions(
+            context as Activity, arrayOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE), writeExt
         )
     }
 
@@ -96,7 +137,7 @@ class FirstFragment : Fragment() {
         requestCode: Int, permissions: Array<String>, grantResults: IntArray
     ) {
         when (requestCode) {
-            pickImage -> {
+            pickImageCode -> {
                 if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(
                         context,
@@ -111,7 +152,7 @@ class FirstFragment : Fragment() {
                     ).show()
                 }
             }
-            captureImage -> {
+            textDetectionCode -> {
                 if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(
                         context,
@@ -132,7 +173,7 @@ class FirstFragment : Fragment() {
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         @Suppress("DEPRECATION") super.onActivityResult(requestCode, resultCode, data)
-        if ((resultCode == RESULT_OK) && (requestCode == pickImage)) {
+        if ((resultCode == RESULT_OK) && (requestCode == pickImageCode)) {
             imageUri = data?.data
             binding.imageviewToBeProcessed.setImageURI(imageUri)
             val imageForMLModel =
@@ -143,7 +184,7 @@ class FirstFragment : Fragment() {
                 error("Null image.")
             }
         }
-        if ((resultCode == RESULT_OK) && (requestCode == captureImage)) {
+        if ((resultCode == RESULT_OK) && (requestCode == textDetectionCode)) {
             binding.imageviewToBeProcessed.setImageURI(imageUri)
             val imageForMLModel =
                 imageUri?.let { it1 -> InputImage.fromFilePath(requireContext(), it1) }
@@ -153,12 +194,25 @@ class FirstFragment : Fragment() {
                 error("Null image.")
             }
         }
+        if ((resultCode == RESULT_OK) && (requestCode == objectDetectionCode)) {
+            binding.imageviewToBeProcessed.setImageURI(imageUri)
+            val imageForMLModel =
+                imageUri?.let { it1 -> InputImage.fromFilePath(requireContext(), it1) }
+            if (imageForMLModel != null) {
+                recognizeObject(imageForMLModel)
+            } else {
+                error("Null image.")
+            }
+        }
     }
 
     private fun recognizeText(image: InputImage) {
         textFromPickedImage = ""
-        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-        recognizer.process(image).addOnSuccessListener { visionText ->
+        processDialog.setMessage("Preparing image...")
+        processDialog.show()
+        val textDetector = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        processDialog.setMessage("Detecting text...")
+        textDetector.process(image).addOnSuccessListener { visionText ->
             for (block in visionText.textBlocks) {
                 val text = block.text
                 textFromPickedImage += text
@@ -169,6 +223,7 @@ class FirstFragment : Fragment() {
                     }
                 }
             }
+            processDialog.dismiss()
             if ((textFromPickedImage == null) || (textFromPickedImage == "")) {
                 binding.textViewResult.text = getString(R.string.text_view_result_text)
             } else {
@@ -177,9 +232,33 @@ class FirstFragment : Fragment() {
             }
         }.addOnFailureListener { e ->
             Toast.makeText(
-                context,
-                "Something went wrong while processing image.",
-                Toast.LENGTH_SHORT
+                context, "Something went wrong while processing image.", Toast.LENGTH_LONG
+            ).show()
+            error(e.message.toString())
+        }
+    }
+
+    private fun recognizeObject(image: InputImage) {
+        textFromPickedImage = ""
+        processDialog.setMessage("Preparing image...")
+        processDialog.show()
+        val options = ImageLabelerOptions.Builder().setConfidenceThreshold(0.95f).build()
+        val objectDetector = ImageLabeling.getClient(options)
+        processDialog.setMessage("Detecting objects...")
+        objectDetector.process(image).addOnSuccessListener { imageLabels ->
+            for (label in imageLabels) {
+                textFromPickedImage += label.text + "\n"
+            }
+            processDialog.dismiss()
+            if ((textFromPickedImage == null) || (textFromPickedImage == "")) {
+                binding.textViewResult.text = getString(R.string.text_view_result_text)
+            } else {
+                binding.textViewResult.text = textFromPickedImage
+                speakOut(binding.textViewResult.text.toString())
+            }
+        }.addOnFailureListener { e ->
+            Toast.makeText(
+                context, "Something went wrong while processing image.", Toast.LENGTH_LONG
             ).show()
             error(e.message.toString())
         }
@@ -189,9 +268,8 @@ class FirstFragment : Fragment() {
         tts = TextToSpeech(context) {
             if (it == TextToSpeech.SUCCESS) {
                 tts.language = Locale.US
-                tts.setSpeechRate(0.5f)
-                @Suppress("DEPRECATION")
-                tts.speak(textForSpeak, TextToSpeech.QUEUE_FLUSH, null)
+                tts.setSpeechRate(1.0f)
+                @Suppress("DEPRECATION") tts.speak(textForSpeak, TextToSpeech.QUEUE_FLUSH, null)
             }
         }
     }
